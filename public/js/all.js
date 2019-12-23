1689243bc2b0e19f1ae5bf9b2beb99c78cea5284
Levar Barrett <emaillevar@gmail.com>
2019-12-23 15:55:17 -0500

    submit button added

---
+++ public/js/all.js
@@ -0,0 +1,27 @@
+$.get("/api/products", function(data) {
+  console.log(data);
+  for (let i = 0; i < data.length; i++) {
+    // Inside the loop...
+
+    // 2. Create a variable named "letterBtn" equal to $("<button>");
+    const tablerow = $("<tr>");
+
+    // 3. Then give each "letterBtn" the following classes: "letter-button" "letter" "letter-button-color".
+    const productId = $("<td>");
+    const productName = $("<td>");
+    const productPrice = $("<td>");
+    const productQuantity = $("<td>");
+    const Buybttn = $();
+    // const productselection = $("<td");
+
+    // 4. Then give each "letterBtn" a data-attribute called "data-letter".
+
+    // 5. Then give each "letterBtns" a text equal to "letters[i]".
+    productId.text(data[i].id);
+    productName.text(data[i].product_name);
+    productPrice.text(data[i].price);
+    productQuantity.text(data[i].stock_quantity);
+
+    tablerow.append(productId, productName, productPrice, productQuantity);
+
+    $("#tbody").app
